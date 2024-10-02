b8532ae09e58b94a305c4ace0be2191734e15f3a
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2024-10-02 16:18:44 -0700

    Update settings.py

--- ikoi/settings.py
+++ ikoi/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),  # Use os.path.join here
     }
 }
 
