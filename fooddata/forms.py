2b67abc513c61547c976b7d6bfd647a44be46350
ksalvo15 <121691678+ksalvo15@users.noreply.github.com>
2024-10-09 11:16:21 -0700

    protein changes

--- fooddata/forms.py
+++ fooddata/forms.py
@@ -27,7 +27,9 @@ class AdvancedSearchExcel(forms.Form):
     specific_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}), required=False)
     sort_column = forms.ChoiceField(choices=[], required=False)
     lunch_item = forms.CharField(max_length=255, required=False)
-    protein = forms.CharField(max_length=255, required=False)
+    #protein = forms.CharField(max_length=255, required=False)
+   
+   
     # Add more fields as needed
 
 
@@ -36,17 +38,4 @@ class TestForm(forms.Form):
 
 
 
-    #date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}), required=False)
-    #number_of_guest_min = forms.IntegerField(required=False, label="DINE IN")
-    #number_of_guests_max = forms.IntegerField(required=False, label="DINE IN")
-    #number_of_volunteers_min = forms.IntegerField(required=False, label="VOLUNTEERS")
-    #number_of_volunteers_max = forms.IntegerField(required=False, label="VOLUNTEERS")
-    #number_of_total_min = forms.IntegerField(required=False, label="VOLUNTEERS")
-    #number_of_total_max = forms.IntegerField(required=False, label="VOLUNTEERS")
-    #totalcost_min = forms.DecimalField(required=False, label="$ AMT CALC", max_digits=10, decimal_places=2)
-    #totalcost_max = forms.DecimalField(required=False, label="$ AMT CALC", max_digits=10, decimal_places=2)
-    #intakecost_min = forms.DecimalField(required=False, label="CASH/ CHECK COLLECTED", max_digits=10, decimal_places=2)
-    #intakescripts_max = forms.DecimalField(required=False, label="CASH/ CHECK COLLECTED", max_digits=10, decimal_places=2)
-    #intakescripts_min = forms.DecimalField(required=False, label="$ AMT  COLL + SCRIPTS", max_digits=10, decimal_places=2)
-    #intakecost_max = forms.DecimalField(required=False, label="$ AMT  COLL + SCRIPTS", max_digits=10, decimal_places=2)
-    #food_item = forms.CharField(required=False, label="Food Name", max_length=255)
+   
